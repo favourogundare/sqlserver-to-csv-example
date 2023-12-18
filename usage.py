8b47edacfcdacef894e0fbf4fb5fa3e33a837b3b
Mohsin Raza <mohsin.doer@gmail.com>
2023-12-18 15:15:32 +0500

    added new scenerio

--- usage.py
+++ usage.py
@@ -53,3 +53,54 @@ my_csv.save()
 
 ## close the source database
 source_db.close()
+
+## Scenerio 3
+
+'''
+1. iterate over table `FILE_MAPPING_SOURCE` and get the `LEGACY_INCIDENT_ID` and `FILENAME` and `FILETYPE`
+2. change FILETYPE to 'application/pdf' if FILETYPE is 'pdf', 'image/jpeg' if FILETYPE is 'jpg' or 'jpeg', 'image/png' if FILETYPE is 'png'
+3. save into target csv `FILE_MAPPING_TARGET` with columns `LEGACY_INCIDENT_ID` and `FILENAME` and `FILETYPE`
+'''
+
+def get_filetype_map(filetype):
+    file_type = filetype.lower().replace('.', '')
+    filetype_map = {
+        'pdf': 'application/pdf',
+        'jpg': 'image/jpeg',
+        'jpeg': 'image/jpeg',
+        'png': 'image/png',
+        'mp4': 'video/mp4',
+        'mp3': 'audio/mpeg',
+        'zip': 'application/zip',
+    }
+    return filetype_map[file_type]
+
+## initialize the source database
+source_db = SourceDB()
+
+## get the columns
+file_mapping_columns = source_db.get_columns('FILE_MAPPING_SOURCE')
+
+## 1. initialize the target csv with specific name and set header using the columns
+my_csv = TargetCSV('new_my_target.csv').set_head([
+    'LEGACY_INCIDENT_ID',
+    'FILENAME',
+    'FILETYPE'
+])
+
+## 2. iterate over the table and append the rows
+for row in source_db.iterate_table('FILE_MAPPING_SOURCE'):
+    ## manipudated_row TODO: do your manipulation here
+    manipulated_row = {}
+    manipulated_row['LEGACY_INCIDENT_ID'] = row['LEGACY_INCIDENT_ID']
+    manipulated_row['FILENAME'] = row['FILENAME']
+    manipulated_row['FILETYPE'] = get_filetype_map(row['FILETYPE'])
+
+    ## add the row to the csv
+    my_csv.append_row(manipulated_row)
+
+## 3. save the csv
+my_csv.save()
+
+## close the source database
+source_db.close()
