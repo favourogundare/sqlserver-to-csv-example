b946f5904083af13fc2f69f307fc10cfe2a650b4
Mohsin Raza <mohsin.doer@gmail.com>
2023-12-18 15:22:59 +0500

    added new scenerio

--- usage.py
+++ usage.py
@@ -135,3 +135,39 @@ for row in source_db.iterate_table('FILE_MAPPING_SOURCE'):
 
 ## 3. save the csv
 my_csv.save()
+
+
+## Scenerio 5
+'''
+1. iterate over table `FILE_MAPPING_SOURCE` and get the `LEGACY_INCIDENT_ID` and `FILENAME` and `ENTER_DATE`
+2. enter date should be in format YYYY-MM-DD
+3. save into target csv `FILE_MAPPING_TARGET` with columns `LEGACY_INCIDENT_ID` and `FILENAME` and `ENTER_DATE`
+'''
+
+## initialize the source database
+source_db = SourceDB()
+
+## get the columns
+file_mapping_columns = source_db.get_columns('FILE_MAPPING_SOURCE')
+
+## 1. initialize the target csv with specific name and set header using the columns
+my_csv = TargetCSV('date_manipulated_target.csv').set_head([
+    'LEGACY_INCIDENT_ID',
+    'FILENAME',
+    'ENTER_DATE'
+])
+
+## 2. iterate over the table and append the rows
+for row in source_db.iterate_table('FILE_MAPPING_SOURCE'):
+    ## manipudated_row TODO: do your manipulation here
+    manipulated_row = {}
+    manipulated_row['LEGACY_INCIDENT_ID'] = row['LEGACY_INCIDENT_ID']
+    manipulated_row['FILENAME'] = row['FILENAME']
+    if row['ENTER_DATE']:
+        manipulated_row['ENTER_DATE'] = row['ENTER_DATE'].strftime('%Y-%m-%d')
+    
+        ## add the row to the csv
+        my_csv.append_row(manipulated_row)
+
+## 3. save the csv
+my_csv.save()
